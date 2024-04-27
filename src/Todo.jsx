cef9f6060f59948491b97f3d94fe113425c6d221
nonrita <2220025@ecc.ac.jp>
2024-04-27 22:46:05 +0900

    タスクの削除機能

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -12,7 +12,7 @@ export const Todo = () => {
         "TODOでした2",
     ]);
 
-    const onChangeTodoText = ( event ) => setTodoText(event.target.value);
+    const onChangeTodoText = (event) => setTodoText(event.target.value);
     
     const onClickAdd = () => {
         if (todoText === "") return;
@@ -20,6 +20,12 @@ export const Todo = () => {
         setIncompleteTodos(newTodos);
         setTodoText("");
     };
+
+    const onClickDelete = (index) => {
+        const newTodos = [...incompleteTodos];
+        newTodos.splice(index, 1);
+        setIncompleteTodos(newTodos);
+    }
 
     return (
         <>
@@ -32,13 +38,13 @@ export const Todo = () => {
                 <p className="title">未完了のTODO</p>
                 <ul>
                     {
-                        incompleteTodos.map((todo) => 
+                        incompleteTodos.map((todo, index) => 
                             (
                                 <li key={todo}>
                                     <div className="list-row">
                                         <p className="todo-item">{todo}</p>
                                         <button>完了</button>
-                                        <button>削除</button>
+                                        <button onClick={() => onClickDelete(index)}>削除</button>
                                     </div>
                                 </li>
                             )
